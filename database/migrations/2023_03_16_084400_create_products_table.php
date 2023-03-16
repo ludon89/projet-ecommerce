4d6e90fec12f8f0783b314fc5f980d4499ba58ca
MikaOcko <moreau.magali.kimberly@gmail.com>
2023-03-16 10:49:41 +0100

    ajout schema products table migrations

--- database/migrations/2023_03_16_084400_create_products_table.php
+++ database/migrations/2023_03_16_084400_create_products_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->decimal('price');
+            $table->text('description');
+            $table->string('picture');
             $table->timestamps();
         });
     }
