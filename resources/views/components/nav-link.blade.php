8df6efd3cd53d7ebc0b0dfeb90775325ad1e164c
MikaOcko <moreau.magali.kimberly@gmail.com>
2023-03-17 12:38:23 +0100

    modification de la navigation & personnalisation du thème tailwinds

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -8,8 +8,8 @@ $classes = ($active ?? false)
 
 @php
 $classes = ($active ?? false)
-            ? 'mt-3 text-gray-600 hover:underline sm:mx-3 sm:mt-0'
-            : 'mt-3 text-gray-600 hover:underline sm:mx-3 sm:mt-0';
+            ? 'mt-3 text-white text-2xl font-semibold font-display hover:underline decoration-yellow decoration-wavy underline-offset-8 sm:mx-3 sm:mt-0'
+            : 'mt-3 text-white text-2xl font-semibold font-display hover:underline decoration-yellow decoration-wavy underline-offset-8 sm:mx-3 sm:mt-0';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,11 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=baumans:400" rel="stylesheet" />
 
+    <!-- CDN google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+
     <!--CSS-->
     {{-- <link rel="stylesheet" href="../../css/main.css"> --}}
 
@@ -19,10 +24,12 @@
     @vite('resources/css/app.css' , 'resources/js/app.js')
 </head>
 
-<body class="font-sans antialiased">
-    <header class="bg-dark">
-        @include('layouts.navigation');
-    </header>
+<body class="font-sans antialiased bg-black">
+    <div x-data="{ cartOpen: false, isOpen: false }">
+        <header>
+            @include('layouts.navigation');
+        </header>
+    </div>
 
     <main>
         {{ $slot }}
