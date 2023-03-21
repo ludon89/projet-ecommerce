d5f4d83568ac33039a486af598f87fced2773e1a
LuDon89 <daudonlucas@gmail.com>
2023-03-21 10:29:12 +0100

    mdr edit fonctionnel

--- resources/views/fireshop/edit.blade.php
+++ resources/views/fireshop/edit.blade.php
@@ -8,7 +8,7 @@
     <div class="max-w-5xl mx-auto p-4 sm:p-6 lg:p-8 text-white">
         <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
             <div class="max-w-5xl mx-auto p-4 sm:p-6 lg:p-8">
-                <form method="POST" action="{{ route('products.update', $product->id) }}">
+                <form method="POST" action="{{ route('products.update', $product->id) }}" enctype="multipart/form-data">
                     @csrf
                     @method('PATCH')
                     <label for="name">Nom :</label><br>
