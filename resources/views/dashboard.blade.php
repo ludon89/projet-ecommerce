33c27556daf5014404135521109407597402a62a
MikaOcko <moreau.magali.kimberly@gmail.com>
2023-03-21 14:11:54 +0100

    maj style

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,20 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+    <x-slot name="slot">
+        <h1 class="font-semibold text-3xl font-display text-yellow mb-5">
+            Dashboard
+        </h1>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-black overflow-hidden shadow-sm sm:rounded-lg border-2 border-yellow ">
+                    <div class="p-6 text-white">
+                        {{ __("You're logged in!") }}
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+
+    </x-slot>
+
+    
 </x-app-layout>
