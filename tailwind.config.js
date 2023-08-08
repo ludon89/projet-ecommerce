af04b25e9408834f1b9a3874a9f5950f7b260b98
ludon89 <daudonlucas@gmail.com>
2023-08-08 10:15:34 +0200

    install prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,27 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-    ],
+  content: [
+    "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+    "./storage/framework/views/*.php",
+    "./resources/views/**/*.blade.php",
+  ],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Lato', 'Helvetica', 'Arial', 'sans-serif'],
-                display: ['Baumans'],
-            },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Lato", "Helvetica", "Arial", "sans-serif"],
+        display: ["Baumans"],
+      },
 
-            colors: {
-                'yellow': '#FFD60A',
-                'green': '#c2e812',
-                'red': '#ff331f',
-            },
-        },
+      colors: {
+        yellow: "#FFD60A",
+        green: "#c2e812",
+        red: "#ff331f",
+      },
     },
+  },
 
-    plugins: [require('@tailwindcss/forms')],
+  plugins: [require("@tailwindcss/forms")],
 };
